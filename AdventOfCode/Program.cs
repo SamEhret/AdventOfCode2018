6e98a65794ef118fc0abcb89fe25560ef61563d3
Sam Ehret <ehret.sam@yahoo.com>
2018-11-27 17:59:33 -0600

    Initial commit

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
