55455c3c92512edfceeb5c794f3ddc152e79ba67
Sam Ehret <ehret.sam@yahoo.com>
2018-12-01 09:02:22 -0600

    Added file reader utility

---
+++ AdventOfCode/Utilities/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode.Utilities
+{
+    public class FileReader
+    {
+        public List<string> ReadFileToList(string filePath)
+        {
+            var fileRead = File.ReadAllLines(filePath);
+            var fileList = new List<string>(fileRead);
+
+            return fileList;
+        }
+    }
+}
