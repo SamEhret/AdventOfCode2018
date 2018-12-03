990cf92e46f611090c8f46bd4eb938898ac94da0
Sam Ehret <ehret.sam@yahoo.com>
2018-12-03 14:01:05 -0600

    Move repeated code out of individual solutions for Day03

--- AdventOfCode/Puzzles/Day03/Solution.cs
+++ AdventOfCode/Puzzles/Day03/Solution.cs
@@ -9,38 +9,8 @@ namespace AdventOfCode.Puzzles.Day03
     {
         public static int Part1()
         {
-            var inputList = InputReader.ReadInputToList("Day03");
-            var regex = new Regex(@"\s+(\d+),(\d+):\s+(\d+)x(\d+)");
-            var squareDict = new Dictionary<int, Dictionary<int, int>>();
+            var squareDict = CreateDictionary();
             var squareSum = 0;
-
-            foreach (var line in inputList)
-            {
-                var matches = regex.Match(line);
-                var top = Convert.ToInt32(matches.Groups[1].Value);
-                var left = Convert.ToInt32(matches.Groups[2].Value);
-                var toBottom = Convert.ToInt32(matches.Groups[3].Value);
-                var toRight = Convert.ToInt32(matches.Groups[4].Value);
-
-                for (var row = top; row <= top + toBottom - 1; row ++)
-                {
-                    for (var column = left; column <= left + toRight - 1; column ++)
-                    {
-                        if (!squareDict.ContainsKey(row))
-                        {
-                            squareDict[row] = new Dictionary<int, int>(){{column, 1}};
-                        }
-                        else if (!squareDict[row].ContainsKey(column))
-                        {
-                            squareDict[row].Add(column, 1);
-                        }
-                        else
-                        {
-                            squareDict[row][column]++;
-                        }
-                    }
-                }
-            }
 
             foreach (var item in squareDict)
             {
@@ -58,38 +28,9 @@ namespace AdventOfCode.Puzzles.Day03
 
         public static int Part2()
         {
+            var squareDict = CreateDictionary();
             var inputList = InputReader.ReadInputToList("Day03");
-            var regex = new Regex(@"(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)");
-            var squareDict = new Dictionary<int, Dictionary<int, int>>();
-
-            foreach (var line in inputList)
-            {
-                var matches = regex.Match(line);
-                var lineNum = Convert.ToInt32(matches.Groups[1].Value);
-                var top = Convert.ToInt32(matches.Groups[2].Value);
-                var left = Convert.ToInt32(matches.Groups[3].Value);
-                var toBottom = Convert.ToInt32(matches.Groups[4].Value);
-                var toRight = Convert.ToInt32(matches.Groups[5].Value);
-
-                for (var row = top; row <= top + toBottom - 1; row++)
-                {
-                    for (var column = left; column <= left + toRight - 1; column++)
-                    {
-                        if (!squareDict.ContainsKey(row))
-                        {
-                            squareDict[row] = new Dictionary<int, int>() {{column, 1}};
-                        }
-                        else if (!squareDict[row].ContainsKey(column))
-                        {
-                            squareDict[row].Add(column, 1);
-                        }
-                        else
-                        {
-                            squareDict[row][column]++;
-                        }
-                    }
-                }
-            }
+            var regex = new Regex(@"\s+(\d+),(\d+):\s+(\d+)x(\d+)");
 
             foreach (var line in inputList)
             {
@@ -119,6 +60,43 @@ namespace AdventOfCode.Puzzles.Day03
             }
 
             return 0;
+        }
+
+        public static Dictionary<int, Dictionary<int, int>> CreateDictionary()
+        {
+            var inputList = InputReader.ReadInputToList("Day03");
+            var regex = new Regex(@"(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)");
+            var squareDict = new Dictionary<int, Dictionary<int, int>>();
+
+            foreach (var line in inputList)
+            {
+                var matches = regex.Match(line);
+                var top = Convert.ToInt32(matches.Groups[2].Value);
+                var left = Convert.ToInt32(matches.Groups[3].Value);
+                var toBottom = Convert.ToInt32(matches.Groups[4].Value);
+                var toRight = Convert.ToInt32(matches.Groups[5].Value);
+
+                for (var row = top; row <= top + toBottom - 1; row++)
+                {
+                    for (var column = left; column <= left + toRight - 1; column++)
+                    {
+                        if (!squareDict.ContainsKey(row))
+                        {
+                            squareDict[row] = new Dictionary<int, int>() { { column, 1 } };
+                        }
+                        else if (!squareDict[row].ContainsKey(column))
+                        {
+                            squareDict[row].Add(column, 1);
+                        }
+                        else
+                        {
+                            squareDict[row][column]++;
+                        }
+                    }
+                }
+            }
+
+            return squareDict;
         }
     }
 }
