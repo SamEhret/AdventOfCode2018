9281a74658af90906f702f79b83befc241812b38
Sam Ehret <ehret.sam@yahoo.com>
2018-12-05 09:20:55 -0600

    Added day05 solution tests

---
+++ AdventOfCodeTests/Puzzles/Day05Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Puzzles.Day05;
+
+namespace AdventOfCodeTests.Puzzles
+{
+    [TestClass]
+    public class Day05Tests
+    {
+        [TestMethod]
+        public void TestPartOneSolutionIsCorrect()
+        {
+            // Arrange
+            // Act
+            var solution = Solution.Part1();
+
+            // Assert
+            Assert.AreEqual(10972, solution);
+        }
+
+        [TestMethod]
+        public void TestPartTwoSolutionIsCorrect()
+        {
+            // Arrange
+            // Act
+            var solution = Solution.Part2();
+
+            // Assert
+            Assert.AreEqual(5278, solution);
+        }
+    }
+}
